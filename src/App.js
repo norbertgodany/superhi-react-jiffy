1cac0bab8103c3db0c9fe538fcf893d0dada5e71
Norbert Godany <norbert.godany@gmail.com>
2019-03-13 17:09:41 +0100

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const Header = () => (
+  <div className="header grid">
+    <h1 className="title">
+      Jiffy
+    </h1>
+  </div>
+)
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      searchTerm: ''
+    }
+  };
+
+  // with modern js we dont need constructor and bind for the this keyword
+  handleChange = event => {
+    // same as const = event.target.value
+    const { value } = event.target
+    this.setState((prevState, props) => ({
+      // we take all our old props and spread them out here
+      ...prevState,
+      // and then overwrite the ones we want after
+      searchTerm: value
+    }))
+    if (value.length >= 3) {
+    }
+  };
+
+  // when we have 2 or more characters in our search box and we have also pressed enter, we then want to run a search
+  handleKeyPress = event => {
+    const { value } = event.target
+    if (value.length >= 3 && event.key === 'Enter') {
+      alert(`Search for ${value}`)
+    }
+  }
+
   render() {
+    // const searchTerm = this.state.searchTerm
+    const {searchTerm} = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="page">
+        <Header />
+        <div className="search grid">
+          <input className="input grid-item" placeholder="Search for a GIF"
+            onChange={this.handleChange}
+            onKeyPress={this.handleKeyPress}
+            value={searchTerm}>
+          </input>
+        </div>
       </div>
     );
   }

392aa5f2195b56fd72b69f7df495b50512279bf0
Norbert Godany <norbert.godany@gmail.com>
2019-03-13 23:27:52 +0100

    before implementing fetch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import loader from './images/loader.svg'
+import close from './images/close-icon.svg'
+
 const Header = () => (
   <div className="header grid">
     <h1 className="title">
@@ -8,17 +11,27 @@ const Header = () => (
   </div>
 )
 
+const UserHint = ({loading, hintText}) => (
+  <div className="user-hint">
+    {/* here we check whether we have a loading state and render out either our sponner or hintText
+    using a tenary operator if else statement*/}
+    {loading ? <img className="block mx-auto" src={loader} /> : hintText}
+  </div>
+)
+
 class App extends Component {
 
   constructor(props) {
     super(props)
 
+    // default states
     this.state = {
-      searchTerm: ''
+      searchTerm: '',
+      hintText: ''
     }
   };
 
-  // with modern js we dont need constructor and bind for the this keyword
+  // with modern js we dont need constructor and bind for the this keyword, just for the state
   handleChange = event => {
     // same as const = event.target.value
     const { value } = event.target
@@ -26,10 +39,10 @@ class App extends Component {
       // we take all our old props and spread them out here
       ...prevState,
       // and then overwrite the ones we want after
-      searchTerm: value
+      searchTerm: value,
+      // we set the hintText only when we have more than 2 characters, else we set it an empty string
+      hintText: value.length > 2 ? `Hit enter to search ${value}` : ''
     }))
-    if (value.length >= 3) {
-    }
   };
 
   // when we have 2 or more characters in our search box and we have also pressed enter, we then want to run a search
@@ -53,6 +66,8 @@ class App extends Component {
             value={searchTerm}>
           </input>
         </div>
+        {/* here we pass UserHint all of our state using a spread*/}
+        <UserHint {...this.state}/>
       </div>
     );
   }
